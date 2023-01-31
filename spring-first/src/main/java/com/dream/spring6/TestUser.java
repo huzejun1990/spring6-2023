b4dfdbfaa981b2e9b81832b3962f9af7380bf0b3
huzejun <huzejun1990>
2023-02-01 02:56:11 +0800

    spring-first中log4j2的使用 bean.xml 以及pom中maven引入 2023-2-1 02:55:16

--- spring-first/src/main/java/com/dream/spring6/TestUser.java
+++ spring-first/src/main/java/com/dream/spring6/TestUser.java
@@ -1,6 +1,9 @@
 package com.dream.spring6;
 
+
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -9,6 +12,9 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  * @Date: 2023/1/31-23:12
  */
 public class TestUser {
+
+    //创建Logger对象
+    private Logger logger = LoggerFactory.getLogger(TestUser.class);
 
     @Test
     public void testUserObject() {
@@ -24,5 +30,20 @@ public class TestUser {
         System.out.print("2:");
         user.add();
 
+        //手动写日志
+        logger.info("### 执行调用成功了。。。");
+
     }
+
+    //反射创建对象
+    @Test
+    public void testUserObject01() throws Exception {
+        // 获取类的Class对象
+        Class clazz = Class.forName("com.dream.spring6.User");
+        //调用方法创建对象
+//        Object o = clazz.newInstance();
+        User user = (User) clazz.getDeclaredConstructor().newInstance();
+        System.out.println(user);
+    }
+
 }
--- spring-first/src/main/java/com/dream/spring6/User.java
+++ spring-first/src/main/java/com/dream/spring6/User.java
@@ -6,6 +6,13 @@ package com.dream.spring6;
  */
 public class User {
 
+    // 无参数构造
+
+
+    public User() {
+        System.out.println("0: 无参数构造执行了。。");
+    }
+
     public void add(){
         System.out.println("add...");
     }
