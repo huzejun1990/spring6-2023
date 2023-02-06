90d7919f170704b791f9eaa01e040940fafa19f1
huzejun <huzejun1990>
2023-02-07 02:35:03 +0800

    spring-ioc-xml（IOC）中基于注解管理bean 全注解开发 2023-2-7 02:34:48

---
+++ spring6-ioc-annotation/src/main/java/com/dream/spring6/config/SpringConfig.java
@@ -0,0 +1,14 @@
+package com.dream.spring6.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Author : huzejun
+ * @Date: 2023/2/7-2:30
+ */
+
+@Configuration //配置类
+@ComponentScan("com.dream.spring6")
+public class SpringConfig {
+}
---
+++ spring6-ioc-annotation/src/main/java/com/dream/spring6/resource/TestUserControllerAnno.java
@@ -0,0 +1,22 @@
+package com.dream.spring6.resource;
+
+import com.dream.spring6.config.SpringConfig;
+import com.dream.spring6.resource.controller.UserController;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @Author : huzejun
+ * @Date: 2023/2/6-21:25
+ */
+public class TestUserControllerAnno {
+
+    public static void main(String[] args) {
+        //加载配置类
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext(SpringConfig.class);
+        UserController controller = context.getBean(UserController.class);
+        controller.add();
+    }
+}
