af6253d16df61f6ecc14106cb53de958f70f87dc
huzejun <huzejun1990>
2023-02-11 03:04:26 +0800

    spring6-aop 面向切面 AOP代理模式 2023-2-11 03:04:19

---
+++ spring6-aop/src/main/java/com/dream/spring6/aop/example/CalculatorImpl.java
@@ -0,0 +1,41 @@
+package com.dream.spring6.aop.example;
+
+/**
+ * @Author : huzejun
+ * @Date: 2023/2/11-2:31
+ */
+
+//基本实现类
+public class CalculatorImpl implements Calculator{
+    @Override
+    public int add(int i, int j) {
+        int result = i + j;
+        System.out.println("方法内部 result = " + result);
+
+        return result;
+    }
+
+    @Override
+    public int sub(int i, int j) {
+        int result = i - j;
+
+        System.out.println("方法内部 result =  " + result);
+
+        return result;
+    }
+
+    @Override
+    public int mul(int i, int j) {
+        int result = i * j;
+        System.out.println("方法内部 result = " + result);
+        return result;
+    }
+
+    @Override
+    public int div(int i, int j) {
+
+        int result = i / j;
+        System.out.println("方法内部 result = " + result);
+        return result;
+    }
+}
