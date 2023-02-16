99d5335d6849929da7ae9bdaa9de9eed0948f95b
huzejun <huzejun1990>
2023-02-17 01:26:08 +0800

    Junit-Spring整合JUnit5和Junit4 2023-2-17 01:25:57

---
+++ spring6-junit/src/main/java/com/dream/spring6/junit/junit5/User.java
@@ -0,0 +1,15 @@
+package com.dream.spring6.junit.junit5;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @Author : huzejun
+ * @Date: 2023/2/16-23:24
+ */
+@Component
+public class User {
+
+    public void run() {
+        System.out.println("user.....");
+    }
+}
