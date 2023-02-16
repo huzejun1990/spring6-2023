1b0e0eccdc5afbe7a035bfd5230a5bfac90c8da6
huzejun <huzejun1990>
2023-02-16 21:51:42 +0800

    spring6-aop 面向切面 基于xml的AOP 五种通知类型 2023-2-16 21:51:32

---
+++ spring6-aop/src/main/java/com/dream/spring6/aop/xmlaop/TestAop.java
@@ -0,0 +1,21 @@
+package com.dream.spring6.aop.xmlaop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @Author : huzejun
+ * @Date: 2023/2/13-3:06
+ */
+public class TestAop {
+
+    @Test
+    public void testAdd() {
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("beanaop.xml");
+        Calculator calculator = context.getBean(Calculator.class);
+        calculator.add(4,3);
+    }
+
+}
